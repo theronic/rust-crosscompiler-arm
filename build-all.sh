75f7cfe4d7019674452c6c9f4185658239eb8bc7
Petrus Theron <github@petrus.co.za>
2019-02-07 12:24:06 +0200

    switch to nightly channel so I can build ARMv6 using nightly Rust

--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-USERNAME=dlecan
+USERNAME=theronic
 IMAGE_PATH=arm
 IMAGE_NAME=$IMAGE_PATH
 IMAGE=$USERNAME/rust-crosscompiler-arm
 
-RUST_CHANNEL=stable
+RUST_CHANNEL=nightly
 
 docker build -t $IMAGE:$RUST_CHANNEL $IMAGE_PATH
 
